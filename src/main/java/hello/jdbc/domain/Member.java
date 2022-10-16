bd940bc609e27a5d707078bf305fd60bdde00bbb
zidol <ajh3166@naver.com>
2022-10-16 19:50:59 +0900

    트랜잭션 적용 예제

--- src/main/java/hello/jdbc/domain/Member.java
+++ src/main/java/hello/jdbc/domain/Member.java
@@ -2,6 +2,8 @@ package hello.jdbc.domain;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class Member {
 
@@ -15,5 +17,18 @@ public class Member {
     public Member(String memberId, int money) {
         this.memberId = memberId;
         this.money = money;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return money == member.money && Objects.equals(memberId, member.memberId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberId, money);
     }
 }
