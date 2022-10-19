943876a5c747133849b5d0c6e08abc296094f126
zidol <ajh3166@naver.com>
2022-10-19 22:19:40 +0900

    트랜잭션 템플릿 예제

--- src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -7,33 +7,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
+ * 트랜잭션 - 트랜잭션 템플릿
  */
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Slf4j
-public class MemberServiceV3_1 {
+public class MemberServiceV3_2 {
 
-    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
 
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
+
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-
-        //트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status);//성공시 커밋
-        } catch (Exception e) {
-            transactionManager.rollback(status); //실패시 롤백
-            throw new IllegalStateException(e);
-        }
-
-        //커밋, 롤백
+        txTemplate.executeWithoutResult((status) -> {
+            //비즈니스 로직
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
      }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
@@ -45,6 +47,7 @@ public class MemberServiceV3_1 {
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
     }
+
 
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
