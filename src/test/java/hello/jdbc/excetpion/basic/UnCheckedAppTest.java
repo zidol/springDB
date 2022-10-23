f595206631086156a766074a58e9cdc83648745e
zidol <ajh3166@naver.com>
2022-10-23 23:29:17 +0900

    예외 포함과 스택 트레이스

--- src/test/java/hello/jdbc/excetpion/basic/UnCheckedAppTest.java
+++ src/test/java/hello/jdbc/excetpion/basic/UnCheckedAppTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.excetpion.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.net.ConnectException;
@@ -7,12 +8,24 @@ import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@Slf4j
 public class UnCheckedAppTest {
 
     @Test
     void unchecked() {
         Controller controller = new Controller();
         assertThatThrownBy(controller::request).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    void printEx() {
+        Controller controller = new Controller();
+
+        try {
+            controller.request();
+        } catch (Exception e) {
+            log.info("ex", e);
+        }
     }
 
     static class Controller {
